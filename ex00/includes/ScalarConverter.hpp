21d8413cc3d3a063a8af9913b93cd91e5e40ed0f
Guillaume <guillox01@gmail.com>
2026-02-22 20:29:45 +0100

    (feat): Add printChar and printInt

--- ex00/includes/ScalarConverter.hpp
+++ ex00/includes/ScalarConverter.hpp
@@ -6,13 +6,16 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 14:34:15 by gpollast          #+#    #+#             */
-/*   Updated: 2026/02/20 09:50:50 by gpollast         ###   ########.fr       */
+/*   Updated: 2026/02/22 20:24:36 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <string>
+
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
 
 enum	e_type
 {
--- ex00/srcs/ScalarConverter.cpp
+++ ex00/srcs/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 10:06:51 by gpollast          #+#    #+#             */
-/*   Updated: 2026/02/20 09:52:32 by gpollast         ###   ########.fr       */
+/*   Updated: 2026/02/22 20:29:13 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <iomanip>
 
 /* ************************************************************************** */
 
@@ -36,22 +37,65 @@ ScalarConverter::~ScalarConverter() {}
 
 // Method
 
-static e_type	getType(const std::string& str)
+static bool	isChar(const std::string& str)
 {
+	if (str.length() == 1)
+		return (true);
 	if (str.length() == 3)
 	{
 		if (str[0] == '\'' && str[2] == '\'')
-			return (CHAR);
+			return (true);
 	}
+	return (false);
+}
+
+static bool	isInt(const std::string& str)
+{
+	for (int i = 0; i < static_cast<int>(str.length()); i++)
+	{
+		if (!std::isdigit(str[i]))
+			return (false);
+	}
+	return (true);
+}
+
+static e_type	getType(const std::string& str)
+{
+	if (isChar(str))
+		return (CHAR);
+	if (isInt(str))
+		return (INT);
 	return (IMPOSSIBLE);
 }
 
-static void	printChar(const std::string& str)
+static void	printChar(char c)
 {
-	if (str[1] < 32 || str[1] > 126)
+	if (c < 32 || c > 126)
 		std::cout << "char: Non Displayable" << std::endl;
 	else
-		std::cout << "char: \'" << str[1] << "\'"  << std::endl;
+		std::cout << "char: \'" << c << "\'"  << std::endl;
+	std::cout << "int: " << static_cast<int>(c) << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(c) << 'f' << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(c) << std::endl;
+}
+
+static void	printInt(const std::string& str)
+{
+	long value = std::strtol(str.c_str(), NULL, 10);
+	if (value < 32 || value > 126)
+		std::cout << "char: Non Displayable" << std::endl;
+	else if (value < INT_MIN || value > INT_MAX)
+	{
+		std::cout << "char: Impossible" << std::endl;
+		std::cout << "int: Impossible" << std::endl;
+	}
+	else
+	{
+		std::cout << "char: \'" << static_cast<char>(value) << "\'"  << std::endl;
+		std::cout << "int: " << static_cast<int>(value) << std::endl;
+	}
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(value) << 'f' << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(value) << std::endl;
 }
 
 void	ScalarConverter::convert(const std::string& str)
@@ -61,10 +105,18 @@ void	ScalarConverter::convert(const std::string& str)
 	switch (type)
 	{
 		case CHAR:
-			printChar(str);
+		{
+			if (str.length() == 1)
+				printChar(str[0]);
+			else
+				printChar(str[1]);
 			break;
+		}
 		case INT:
+		{
+			printInt(str);
 			break;
+		}
 		case FLOAT:
 			break;
 		case DOUBLE:
